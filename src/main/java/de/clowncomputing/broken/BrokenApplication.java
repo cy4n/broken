408dc1786553fdbf8fc39c6eada01c9f0c66e786
Christian Kühn <chris@clowncomputing.de>
2019-04-23 16:48:11 +0200

    init: app from start.spring.io with some starters

---
+++ src/main/java/de/clowncomputing/broken/BrokenApplication.java
@@ -0,0 +1,13 @@
+package de.clowncomputing.broken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrokenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/clowncomputing/broken/BrokenApplicationTests.java
@@ -0,0 +1,16 @@
+package de.clowncomputing.broken;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BrokenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
